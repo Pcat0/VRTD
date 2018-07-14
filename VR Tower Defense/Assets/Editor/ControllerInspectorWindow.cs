e9e1ec4081e272e5d93cbcf09003d256c292de32
Patrick Ambli <patrick.ambli@gmail.com>
2018-07-13 22:46:26 -0500

    Controller Inspector

--- VR Tower Defense/Assets/Editor/ControllerInspectorWindow.cs
+++ VR Tower Defense/Assets/Editor/ControllerInspectorWindow.cs
@@ -5,8 +5,8 @@ using XRPlus;
 
 public class ControllerInspectorWindow : EditorWindow {
     public XRControllerHand ActiveHand = XRControllerHand.Left;
-    Image trackpad;
-    Image crosshairs;
+    Texture trackpad;
+    Texture crosshairs;
     public XRController ActiveController {
         get {
             return XRPlusHandler.Controllers[ActiveHand];
@@ -15,13 +15,14 @@ public class ControllerInspectorWindow : EditorWindow {
     [MenuItem("Tools/Controller Inspector/Open")]
     public static void ShowWindow() {
         ControllerInspectorWindow window = CreateInstance<ControllerInspectorWindow>();
-        window.Load();
         window.titleContent = new GUIContent("VR Controller");
         window.Show();
     }
 
-    public void Load() {
-
+    void Awake() {
+        trackpad = EditorGUIUtility.LoadRequired("Trackpad.png") as Texture;
+        crosshairs = EditorGUIUtility.LoadRequired("Crosshairs.png") as Texture;
+        Debug.Log(trackpad);
     }
     void OnGUI() {
         DrawToolBar();
@@ -35,10 +36,15 @@ public class ControllerInspectorWindow : EditorWindow {
         GUILayout.Label(ActiveController.GripAxis.ToString() + " Grip (Axis)");
         GUILayout.Label(ActiveController.Horizontal.ToString() + " Horizontal");
         GUILayout.Label(ActiveController.Vertical.ToString() + " Vertical");
+
+        DrawTrackpad(200, 200, ActiveController.Horizontal, ActiveController.Vertical, ActiveController.TrackpadTouch);
     }
-    void DrawTrackpad() {
-        GUILayout.BeginArea(new Rect(0, 0, 100, 100), image);
-        GUILayout.
+    void DrawTrackpad(float width, float height, float x, float y, bool showCrosshairs) {
+        GUI.BeginGroup(new Rect(Screen.width / 2 - width / 2, Screen.height / 2 - height / 2, width, height));
+        GUI.Box(new Rect(0, 0, width, height), trackpad);
+        if (showCrosshairs)
+            GUI.Box(new Rect(width * (x * .5f *.5f + .5f) - 10, height * (y * .5f * .5f + .5f) - 10, 20, 20), crosshairs);
+        GUI.EndGroup();
     }
     void DrawToolBar() {
         GUILayout.BeginHorizontal(EditorStyles.toolbar);
